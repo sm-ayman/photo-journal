142c92cc20454e4ee6003a76d72fed12ead83ef6
sm_ayman <sultanmdayman@gmail.com>
2025-05-18 23:54:52 +0600

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import About from './components/About'
-import Gallery from './components/Gallery'
-import Projects from './components/Projects'
-import Footer from './components/Footer'
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Gallery from "./components/Gallery";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
-import 'bootswatch/dist/brite/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min'
-import './index.css'
+import "bootswatch/dist/brite/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "./index.css";
 
 function App() {
-
   return (
-    <div className='bg-lines'>
+    <div className="bg-lines">
       <Navbar />
       <Hero />
       <About />
       <Gallery />
       <Projects />
+      <hr className="border border-light w-100" />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faInstagram,
+  faXTwitter,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="container my-5 d-flex flex-column align-items-center justify-content-center">
+      <h3>「 PhotoJournal 」</h3>
+      <ul className="list-unstyled d-flex gap-5 border border-1 border-primary-subtle p-2">
+        <li>
+          <a href="https://www.facebook.com/smayman98">
+            <FontAwesomeIcon icon={faFacebookF} size="lg" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/sm_ayman/">
+            <FontAwesomeIcon icon={faInstagram} size="lg" />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <FontAwesomeIcon icon={faXTwitter} size="lg" />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <FontAwesomeIcon icon={faWhatsapp} size="lg" />
+          </a>
+        </li>
+      </ul>
+      <p className="fs-5">Created By <a href="https://sm-ayman.netlify.app/" className="text-info-emphasis">Sultan Md. Ayman</a> | All Rights Reserved | © 2025</p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
